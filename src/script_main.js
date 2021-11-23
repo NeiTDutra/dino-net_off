9e5c3cc4127534f829ef02510f859737d1c95441
nei <nei.thomass@gmail.com>
2021-11-23 12:47:35 -0300

    implementa cactus

--- src/script_main.js
+++ src/script_main.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById('dino');
+const cenario = document.getElementById('cena');
 
 let isJumping = false;
 let isGameOver = false;
@@ -39,25 +40,26 @@ function jump() {
                     dinoPosition -= 20;
                     dino.style.bottom = dinoPosition + '%';
                 }
-            }, 20);
+            }, 60);
         }
         else {
 
             dinoPosition += 20;
             dino.style.bottom = dinoPosition + '%';
         }
-    }, 20);
+    }, 60);
 }
 
 function createCactus() {
 
-    const cactus = document.getElementById('cactus');
-    let cactusPosition = 1000;
+    const cactus = document.createElement('div');
+    let cactusPosition = 1100;
     let randomCactus = Math.random() * 6000;
 
     if(isGameOver) return;
 
     cactus.classList.add('cactus');
+    cenario.appendChild(cactus);
     cactus.style.left = cactusPosition + 'px';
 
     let leftTime = setInterval( () => {
@@ -65,9 +67,23 @@ function createCactus() {
         if(cactusPosition < -60) {
 
             clearInterval(leftTime);
-            
+            cenario.removeChild(cactus);
         }
-    });
+        else if(cactusPosition > 0 && cactusPosition < 350 && dinoPosition < 80) {
+
+            clearInterval(leftTime);
+            isGameOver = true;
+            document.getElementById('cena').innerHTML = '<h1 class="gameOverText">Fim de jogo!!</h1>';
+        }
+        else {
+
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomCactus);
 }
 
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
