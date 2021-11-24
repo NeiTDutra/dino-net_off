6569d39a92c24080390c19f45bbeb5f6f66da1ca
nei <nei.thomass@gmail.com>
2021-11-24 10:00:28 -0300

    implementa cenario gif

--- src/script_main.js
+++ src/script_main.js
@@ -46,7 +46,7 @@ function jump() {
                     dinoPosition -= 3;
                     dino.style.bottom = dinoPosition + '%';
                 }
-            }, 40);
+            }, 60);
         }
         else {
 
@@ -83,11 +83,10 @@ function createCactus() {
         }
         else if(cactusPosition > 0 && cactusPosition < 35 && dinoPosition < 35) {
 
-            timer = false;
             clearInterval(leftTime);
             document
                 .getElementById('cena')
-                .innerHTML = '<h1 class="gameOverText">Fim de jogo!!</h1><a href="index.html">Recarregar</a>';
+                .innerHTML = '<h1 class="gameOverText">Fim de jogo!!</h1><p class="text-center"><a href="index.html">Recarregar</a></p>';
             isGameOver = true;
 
         }
@@ -101,70 +100,69 @@ function createCactus() {
     setTimeout(createCactus, randomCactus);
 }
 
-function timeClock() {
+let s = 0;
+let m = 0;
+let h = 0;
 
-    let s = 0;
-    let m = 0;
-    let h = 0;
+var set = setInterval( () => {
 
-    if(timer) {
-
-        var set = setInterval( () => {
-
-            s += 1;
-            
-            if(s == 60) s = 0;
-
-            if(s < 10) {
-
-                segundo.innerText = '0'+s;
-            }
-            else {
-
-                segundo.innerText = s;
-            }
-            
-            if(s == 0) {
+    s += 1;
     
-                m += 1;
+    if(s == 60) s = 0;
 
-                if(m == 60) m = 0;
+    if(s < 10) {
 
-                if(m < 10) {
-
-                    s = 0;
-                    minuto.innerText = '0'+m;
-                }
-                else {
-
-                    s = 0;
-                    minuto.innerText = m;
-                }
-            }
-            if(m == 59) {
-    
-                h += 1;
-
-                if(h == 24) h = 0;
-
-                if(h < 10) {
-
-                    m = 0;
-                    hora.innerText = '0'+h;
-                }
-                else {
-
-                    m = 0;
-                    hora.innerText = h;
-                }
-            }
-        }, 1000);
+        segundo.innerText = '0'+s;
     }
     else {
 
-        clearInterval(set);
-        return;
+        segundo.innerText = s;
     }
+    
+    if(s == 0) {
+
+        m += 1;
+
+        if(m == 60) m = 0;
+
+        if(m < 10) {
+
+            s = 0;
+            minuto.innerText = '0'+m;
+        }
+        else {
+
+            s = 0;
+            minuto.innerText = m;
+        }
+    }
+    if(m == 59) {
+
+        h += 1;
+
+        if(h == 24) h = 0;
+
+        if(h < 10) {
+
+            m = 0;
+            hora.innerText = '0'+h;
+        }
+        else {
+
+            m = 0;
+            hora.innerText = h;
+        }
+    }
+}, 1000);
+
+function timeClock() {
+    
+    if(timer) {
+
+        return set;
+    }
+
+    setTimeout(set);
 }
 
 window.onload = timeClock();
